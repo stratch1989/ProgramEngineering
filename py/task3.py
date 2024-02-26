b8d161bc1c7d0346edf8b52021ab1fec52b5ff41
Ruslan <shaydullin.r.d@yandex.ru>
2024-02-26 20:04:12 +0500

    удалил лишнее

--- py/task3.py
+++
@@ -1,9 +0,0 @@
-val = int(input())
-if val >= 0 and val <= 3:
-    print("Первый диапозон")
-elif val > 3 and val < 6:
-    print("Второй диапозон")
-elif val >= 6 and val < 11:
-    print("Третий диапозон")
-else:
-    print("Число не входит ни в один диапозон")
