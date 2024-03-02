ee9554b0c881e2bec4b9e8738b5a562a6017d1e9
Ruslan <shaydullin.r.d@yandex.ru>
2024-03-02 14:08:47 +0500

    add task1

--- py/task1.py
+++ py/task1.py
@@ -1,17 +1,7 @@
-f = open('task1.txt', 'r')
-text = str(f.readlines()).replace(',','').replace('.','').replace('\\n','').replace("'",'').lower()
-text1 = list(text.split(' '))
-example = 0
-word = ""
-hWord = 0
-for i in text1:
-    for y in text1:
-        if i == y:
-            example += 1
-        if example > hWord:
-            hWord = example
-            word = i
-    example = 0
-print("Всего слов:", len(text1))
-print(f'Самое часто встречаемое слово - "{word}", оно встречается {hWord} раз')
-f.close
+class Hobbie:
+    def __init__(self, type, name):
+        self.type = type
+        self.name = name
+
+my_car = Hobbie("Sport", "Snowboarding")
+print(f'Тип хобби - {my_car.type}, название хобби - {my_car.name}')
